e5d03c59a7aa5ceab5f36a43d34b248429b31a93
Kai Wilken <kai.wilken@cassini.de>
2014-03-25 10:23:59 +0100

    Deactivated HttpRepoUrlTest -> checkConnection
    Only happy case deactivated, does not work in a proxy environment.

--- test/fast/com/tw/go/plugin/util/HttpRepoURLTest.java
+++ test/fast/com/tw/go/plugin/util/HttpRepoURLTest.java
@@ -8,10 +8,10 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class HttpRepoURLTest {
-    @Test
-    public void shouldNotThrowExceptionIfCheckConnectionToTheRepoPasses() {
-        RepoUrl.create("http://google.com", null, null).checkConnection();
-    }
+    //@Test
+    //public void shouldNotThrowExceptionIfCheckConnectionToTheRepoPasses() {
+    //    RepoUrl.create("http://google.com", null, null).checkConnection();
+    //}
 
     @Test
     public void shouldFailCheckConnectionToTheRepoWhenUrlIsNotReachable() {
